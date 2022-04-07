e91443fe152ee05fbdc63873e655d6f87d378c51
Butters <k.butterfield08@gmail.com>
2022-04-06 19:54:05 -0500

    Initial Commit

---
+++ src/main/java/com/butterfield/farmtracker/FarmtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.butterfield.farmtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/butterfield/farmtracker/FarmtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butterfield.farmtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
